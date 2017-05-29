7d410e22a43ce54118ee396f3aad25a24cd501ad
Edward Stevinson <edward.stevinson@hotmail.com>
2017-05-29 12:59:26 +0100

    Created models and references

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
+  has_many :reviews
+end
---
+++ app/models/booking.rb
@@ -0,0 +1,5 @@
+class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :article
+  has_one :review
+end

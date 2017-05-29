0f3e707a12787b741ee5a43e7f12418f6b92b27f
Edward Stevinson <edward.stevinson@hotmail.com>
2017-05-29 16:34:10 +0100

    Update bookings_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,5 @@
+# Controller that implements the logic for the bookings of the articles. Articles are booked 
+# for a particular time frame and charged by a day rate.
 class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
@@ -15,6 +17,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.article = Article.find(params[:article_id])
+    # If the booking is valid it will save to the db, otherwise retain info inputted
     if @booking.save
       redirect_to user_path(@booking.article)
     else
@@ -24,6 +27,7 @@ class BookingsController < ApplicationController
 
   private
 
+  # Whitelist and return a hash of the params required
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :price)
   end

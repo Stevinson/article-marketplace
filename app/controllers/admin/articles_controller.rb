aa39377b54cd710062218ce9ba91fc1fbfaf462c
Edward Stevinson <edward.stevinson@hotmail.com>
2017-05-29 16:30:26 +0100

    Comments

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,4 +1,6 @@
+# Controller for the articles that a user has created themselves
 class Admin::ArticlesController < ApplicationController
+  # Get access to the articles that both belong to the current user
   def index
     @articles = current_user.articles
   end
